cc6bdd6b222fcc18f92059a1ae69b5b3c9b5322d
Maggich <mvahaevich@mail.ru>
2026-07-13 21:49:46 +0500

    Docker_connect

--- frontend/front/vite.config.ts
+++ frontend/front/vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true, // необходимо для работы в Docker
+    port: 5173,
+    watch: {
+      usePolling: true, // гарантирует обновление страниц (HMR) внутри Docker на Windows/Mac
+    },
+  },
 })
